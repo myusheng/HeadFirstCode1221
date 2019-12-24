afa933369f1a11d3cff19f221a66eda3d387ca0d
e <271328795@qq.com>
2019-12-24 21:14:26 +0800

    add Chapter07

---
+++ src/main/java/Chapter07/Chapter07_Ducks/Duck.java
@@ -0,0 +1,17 @@
+/**
+ * Adapter mode
+ */
+package Chapter07_Ducks;
+
+/**
+ * @author mengy
+ * Date:2019-12-24
+ */
+public interface Duck {
+	
+	// Quack behavior
+	public void quack();
+	
+	//Fly behavior
+	public void fly();
+}
---
+++ src/main/java/Chapter07/Chapter07_enumIterAdapter/IteratorEnumeration.java
@@ -0,0 +1,30 @@
+/**
+ * package Chapter07_enumIterAdapter
+ * Adapter mode
+ */
+package Chapter07_enumIterAdapter;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+/**
+ * @author MY
+ * Date:2019-12-24
+ */
+public class IteratorEnumeration implements Enumeration {
+	Iterator iterator;
+ 
+	public IteratorEnumeration(Iterator iterator) {
+		this.iterator = iterator;
+	}
+	
+	@Override
+	public boolean hasMoreElements() {
+		return iterator.hasNext();
+	}
+	
+	@Override
+	public Object nextElement() {
+		return iterator.next();
+	}
+}
